99a2f76106576a28b1e0d3b53355e8fa36f1ce9f
SathuSid <sisindhriy123@gmail.com>
2025-05-16 00:31:44 +0530

    roboshop terraform

--- 02-variable/main.tf
+++ 02-variable/main.tf
@@ -14,3 +14,39 @@ output "URL" {
 output "URL-combo" {
   value = "URL is ${var.URL}"
 }
+
+
+
+##list
+
+variable "list_1" {
+  default = [
+    10,
+    20,
+    "sid",
+    true
+  ]
+}
+
+
+variable "map_1" {
+  default = {
+    aws = {
+      trainer = "sathu"
+      Fee = 3500
+    }
+    azure = {
+      trainer = "sid"
+      Fee = 3000
+    }
+  }
+}
+
+
+output "list_one_var" {
+  value = var.list_1[0]
+}
+
+output "map_one_var" {
+  value = var.map_1.aws[trainer]
+}
